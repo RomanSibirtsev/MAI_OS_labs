a5dae5b6ec5ec0e7fcf71515c4247f21c6fcf6de
Roman Sibirtsev <rom_a04@mail.ru>
2023-09-22 21:48:05 +0300

    add pipes

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -3,28 +3,38 @@
 #include <unistd.h>
 
 int main() {
-    int status, fd1[2], fd2[2];
+    int status, fd1[2], fd2[2], connect[2];
     pid_t pid;
     std::string s;
 
     pipe(fd1);
     pipe(fd2);
+    pipe(connect);
 
     pid = fork();
     if (pid == -1) {
 
-    } else if (pid == 0) {
+    } else if (pid == 0) { // child 1
+        close(connect[0]);
+        close(fd1[1]);
+        dup2(connect[1], STDOUT_FILENO);
         execlp();
     }
     pid = fork();
     if (pid == -1) {
 
-    } else if (pid == 0) {
+    } else if (pid == 0) { // child 2
+        close(connect[1]);
+        close(fd2[0]);
+        dup2(connect[0], STDIN_FILENO);
         execlp();
     }
 
-    while (std::cin >> s) {
+    if (pid != 0) {
 
+        while (std::cin >> s) {
+
+        }
     }
 
     waitpid(-1, &status, 0);
